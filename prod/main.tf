265842836519cbbff558a8c8ff7763528d237782
C0deWave <jamg123123@naver.com>
2022-10-02 17:54:45 +0900

    Feat: vpc 모듈화

--- prod/main.tf
+++ prod/main.tf
@@ -13,3 +13,13 @@ terraform {
   }
 }
 
+module "stage_vpc" {
+  source               = "../module/vpc"
+  vpc_cidr             = "10.100.0.0/16"
+  vpc_name             = "lucky_tree_proc_vpc"
+  public_subnet1_cidr  = "10.100.1.0/24"
+  public_subnet2_cidr  = "10.100.2.0/24"
+  private_subnet1_cidr = "10.100.3.0/24"
+  private_subnet2_cidr = "10.100.4.0/24"
+  use_nat_gateway      = true
+}
--- stage/main.tf
+++ stage/main.tf
@@ -13,3 +13,14 @@ terraform {
   }
 }
 
+
+module "stage_vpc" {
+  source               = "../module/vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  vpc_name             = "lucky_tree_stage_vpc"
+  public_subnet1_cidr  = "10.0.1.0/24"
+  public_subnet2_cidr  = "10.0.2.0/24"
+  private_subnet1_cidr = "10.0.3.0/24"
+  private_subnet2_cidr = "10.0.4.0/24"
+  use_nat_gateway      = true
+}
