edc4c30001cef93b1ac694f93bc7eeb9988e3fa4
C0deWave <jamg123123@naver.com>
2022-10-23 23:18:40 +0900

    Fix: vpc module 버전 번경 및 테라폼 버전 명시

--- stage/main.tf
+++ stage/main.tf
@@ -8,14 +8,18 @@ provider "aws" {
 # terraform state pull을 하면 s3에 있는 state 값을 볼 수 있습니다.
 # terraform init -backend-config=backend.hcl
 terraform {
+  # 정확히 1.2.7버전의 테라폼을 이용합니다.
+  required_version = "1.2.7"
   backend "s3" {
     key = "stage/terraform.tfstate"
   }
 }
 
 
+
+
 module "stage_vpc" {
-  source               = "github.com/C0deWave/lucky_tree_terraform_module.git//vpc?ref=v0.0.1"
+  source               = "github.com/C0deWave/lucky_tree_terraform_module.git//vpc?ref=v_vpc_1.0.1"
   vpc_cidr             = "10.0.0.0/16"
   vpc_name             = "lucky_tree_stage_vpc"
   public_subnet1_cidr  = "10.0.1.0/24"
@@ -24,8 +28,8 @@ module "stage_vpc" {
   private_subnet2_cidr = "10.0.4.0/24"
   use_nat_gateway      = true
 }
-# resource "aws_instance" "name" {
-#   ami = "ami-0c76973fbe0ee100c"
-#   instance_type = "t2.micro"
-#   subnet_id = module.stage_vpc.public_subnet_id[0]
-# }
+
+output "module_stage_vpc" {
+  value = module.stage_vpc
+}
+
